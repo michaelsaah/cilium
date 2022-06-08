cfd3b5d621e6f2e9771c95a30997e726502acd4a
Joe Stringer <joe@cilium.io>
2022-06-07 22:57:33 -0700

    ipcache: Add new {Upsert,Remove}Prefixes() APIs
    These new asynchronous APIs can be used by the policy engine to ensure
    that the ipcache allocates a new local (CIDR) identity for a particular
    prefix.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/ipcache/ipcache.go
+++ pkg/ipcache/ipcache.go
@@ -12,8 +12,10 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/ipcache/types"
 	ipcacheTypes "github.com/cilium/cilium/pkg/ipcache/types"
 	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/labels/cidr"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
@@ -444,6 +446,28 @@ func (ipc *IPCache) RemoveMetadata(prefix netip.Prefix, resource ipcacheTypes.Re
 	ipc.metadata.remove(prefix, resource, aux...)
 	ipc.metadata.Unlock()
 	ipc.metadata.enqueuePrefixUpdates(prefix)
+	ipc.TriggerLabelInjection()
+}
+
+func (ipc *IPCache) UpsertPrefixes(prefixes []netip.Prefix, src source.Source, resource types.ResourceID) {
+	ipc.metadata.Lock()
+	// TODO: Expand the labels lazily
+	for _, p := range prefixes {
+		ipc.metadata.upsertLocked(p, src, resource, cidr.GetCIDRLabels(p))
+		ipc.metadata.enqueuePrefixUpdates(p)
+	}
+	ipc.metadata.Unlock()
+	ipc.TriggerLabelInjection()
+}
+
+func (ipc *IPCache) RemovePrefixes(prefixes []netip.Prefix, src source.Source, resource types.ResourceID) {
+	ipc.metadata.Lock()
+	// TODO: Expand the labels lazily
+	for _, p := range prefixes {
+		ipc.metadata.remove(p, resource, cidr.GetCIDRLabels(p))
+		ipc.metadata.enqueuePrefixUpdates(p)
+	}
+	ipc.metadata.Unlock()
 	ipc.TriggerLabelInjection()
 }
 
